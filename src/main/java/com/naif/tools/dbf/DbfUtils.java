edfa0d3e0b18cde5d08879203ea188b065a7a4f7
nelsonfer1 <nelsonjava@gmail.com>
2014-07-05 15:05:04 -0500

    first commit

--- src/main/java/com/naif/tools/dbf/DbfUtils.java
+++ src/main/java/com/naif/tools/dbf/DbfUtils.java
@@ -11,56 +11,56 @@ public class DbfUtils {
 
     public static void beanDbf(String sfile,String sClass) {
 
-    DBFFile dbf = new DBFReader().readDBFFile(sfile);
+        DBFFile dbf = new DBFReader().readDBFFile(sfile);
 
-    if (dbf == null){
-       System.out.println( "No existen Registros" );
-       return;
-    }
+        if (dbf == null){
+           System.out.println( "No existen Registros" );
+           return;
+        }
 
-    DBFRecord registro = dbf.getRecord("CCODIGOEST","214185");
-    if (registro == null){
-       System.out.println( "Registro no existe" );
-       return;
-    }
+        DBFRecord registro = dbf.getRecord("CCODIGOEST","214185");
+        if (registro == null){
+           System.out.println( "Registro no existe" );
+           return;
+        }
 
-    if (!registro.isActive()){
-       System.out.println( "Registro Eliminado" );
-       return;
-    }
+        if (!registro.isActive()){
+           System.out.println( "Registro Eliminado" );
+           return;
+        }
 
 
-    System.out.println("package com.naif.sima.dbfs;");
-    System.out.println("");
-    System.out.println("import java.util.*;");
-    System.out.println("");
-    System.out.println("import com.naif.tools.dbffile.DBFFieldDescriptor;");
-    System.out.println("import com.naif.tools.dbffile.DBFFile;");
-    System.out.println("import com.naif.tools.dbffile.DBFReader;");
-    System.out.println("import com.naif.tools.dbffile.DBFRecord;");
-    System.out.println("");
-    System.out.println("public class "+sClass+" implements java.io.Serializable {");
-    System.out.println("");
+        System.out.println("package com.naif.sima.dbfs;");
+        System.out.println("");
+        System.out.println("import java.util.*;");
+        System.out.println("");
+        System.out.println("import com.naif.tools.dbffile.DBFFieldDescriptor;");
+        System.out.println("import com.naif.tools.dbffile.DBFFile;");
+        System.out.println("import com.naif.tools.dbffile.DBFReader;");
+        System.out.println("import com.naif.tools.dbffile.DBFRecord;");
+        System.out.println("");
+        System.out.println("public class "+sClass+" implements java.io.Serializable {");
+        System.out.println("");
 
         for (DBFFieldDescriptor fd : dbf.getFieldDescs()) {
 
-        switch(fd.getFieldType()) {
-            case 'C':
-                System.out.println("    private String " + BeanUtils.attribute(fd.getField())); break;
-            case 'N':
-                if (fd.getDecfieldLenght() == 0) {
-                    System.out.println("    private int " + BeanUtils.attribute(fd.getField()));
-                } else {
-                    System.out.println("    private float " + BeanUtils.attribute(fd.getField()));
-                }
-                break;
-            case 'D':
-                System.out.println("    private Date " + BeanUtils.attribute(fd.getField())); break;
-            case 'L':
-                System.out.println("    private boolean " + BeanUtils.attribute(fd.getField())); break;
-            default: break;
-        }
-        }
+            switch(fd.getFieldType()) {
+                case 'C':
+                   System.out.println("    private String " + BeanUtils.attribute(fd.getField())); break;
+                case 'N':
+                   if (fd.getDecfieldLenght() == 0) {
+                      System.out.println("    private int " + BeanUtils.attribute(fd.getField()));
+                   } else {
+                      System.out.println("    private float " + BeanUtils.attribute(fd.getField()));
+                   }
+                   break;
+                case 'D':
+                  System.out.println("    private Date " + BeanUtils.attribute(fd.getField())); break;
+                case 'L':
+                  System.out.println("    private boolean " + BeanUtils.attribute(fd.getField())); break;
+                default: break;
+            } // switch
+        } // for
 
         System.out.println("");
 
@@ -116,30 +116,30 @@ public class DbfUtils {
 
 
             default: break;
-        }
+        } // switch
         System.out.println("");
-        }
+        } // for
 
-    System.out.println("public ArrayList<DBFRecord> getRegistros() {");
-    System.out.println("    return registros;");
-    System.out.println("}");
-    System.out.println("");
+        System.out.println("public ArrayList<DBFRecord> getRegistros() {");
+        System.out.println("    return registros;");
+        System.out.println("}");
+        System.out.println("");
 
-    System.out.println("public ArrayList<String> getIds(String id) {");
-    System.out.println("");
-    System.out.println("    ArrayList<String> ids = new ArrayList<String>();");
-    System.out.println("");
-    System.out.println("    for (DBFRecord registro : registros) {");
-    System.out.println("        ids.add((String)registro.getField(id));");
-    System.out.println("    }");
-    System.out.println("");
-    System.out.println("    return ids;");
-    System.out.println("}");
-    System.out.println("");
+        System.out.println("public ArrayList<String> getIds(String id) {");
+        System.out.println("");
+        System.out.println("    ArrayList<String> ids = new ArrayList<String>();");
+        System.out.println("");
+        System.out.println("    for (DBFRecord registro : registros) {");
+        System.out.println("        ids.add((String)registro.getField(id));");
+        System.out.println("    }");
+        System.out.println("");
+        System.out.println("    return ids;");
+        System.out.println("}");
+        System.out.println("");
 
-    System.out.println("public void seek(String cCodigoEst) {");
-    System.out.println("    registro = dbf.getRecord(\"CCODIGOEST\",cCodigoEst);");
-    System.out.println("");
+        System.out.println("public void seek(String cCodigoEst) {");
+        System.out.println("    registro = dbf.getRecord(\"CCODIGOEST\",cCodigoEst);");
+        System.out.println("");
 
         for (DBFFieldDescriptor fd : dbf.getFieldDescs()) {
         switch(fd.getFieldType()) {
@@ -179,12 +179,12 @@ public class DbfUtils {
                    break;
 
             default: break;
-        }
+        } // switch
         System.out.println("");
-        }
+        } // for
 
     System.out.println("}");
 
-    }
+    } // beanDbf
 
 }
